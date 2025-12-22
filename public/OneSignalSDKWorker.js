2cbf5fdf98c3f60bbd9b23ccff84238f63f759e4
Douglas-InvestBet <douglasnoticias@gmail.com>
2025-12-22 08:24:08 -0300

    Delete public/OneSignalSDKWorker.js

--- public/OneSignalSDKWorker.js
+++
@@ -1,6 +0,0 @@
-// OneSignal Service Worker (Corrigido para v16)
-// Este arquivo deve apenas importar o script principal do OneSignal.
-// A chamada OneSignal.init() e os listeners customizados foram removidos
-// para corrigir o erro "OneSignal is not defined" e garantir a compatibilidade com o SDK v16.
-
-importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
